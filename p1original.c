7120d86efc2dc92cef4462447dd0275b6d8f58a9
bhanuteja199 <94587545+Bhanuteja199@users.noreply.github.com>
2022-02-08 13:03:48 +0000

    p1 completed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
+#include<stdio.h>
+void input(float*base, float*height)
+{
+  printf("Enter the value of BASE:-\n");
+  scanf("%f",base);
+  printf("Enter the value of HEIGHT:-\n");
+  scanf("%f",height);
+}
 
+void find_area(float base, float height,float*area)
+{
+
+  *area=0.5*base*height;
+}
+
+void output(float a, float b,float area)
+
+{
+  printf("The Area of trinagle is %f",area);
+}
+int main()
+{
+  float base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
